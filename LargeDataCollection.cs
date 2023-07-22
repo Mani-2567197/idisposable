0245081ca15c5de06e48c9e63ff380a54c027ec5
unknown <manikanta.official1408@gmail.com>
2023-07-22 15:05:06 +0530

    idisposable

---
+++ LargeDataCollection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_collections
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<string> list = null;
+        bool disposed;
+        public int count;
+        
+
+        public LargeDataCollection(List<string> list)
+        {
+            this.list = list;
+            Console.WriteLine("List Created");
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    list.Clear();
+
+                }
+            }
+            list = null;
+            disposed = true;
+           
+            Console.WriteLine($"Collection {list} has been disposed");
+          
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public string Push(string item)
+        {
+            list.Add(item);
+            return item;
+        }
+        public void Read()
+        {
+            foreach (string i in list)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.WriteLine("\n");
+        }
+        public void Delete(string item)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+                Console.WriteLine($"{item} is deleted successfully from the list");
+            }
+            else
+            {
+                Console.WriteLine($"{item} is not in the list");
+            }
+            
+        }
+        public void getItem(string item)
+        {
+            if (list.Contains(item))
+            {
+                Console.WriteLine($"{item} is available in the list");
+            }
+            else
+            {
+                Console.WriteLine($"{item} is not available in the List");
+            }
+        }
+        ~LargeDataCollection()
+        {
+            Dispose(false);
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> l1 = new List<string>();
+
+            using (LargeDataCollection dc = new LargeDataCollection(l1))
+            {
+
+                Console.WriteLine("Enter how many items do u wanna add");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter items to insert into list");
+                for (int i = 0; i < num; i++)
+                {
+                    string add = Console.ReadLine();
+                    dc.Push(add);
+                }
+                dc.Read();
+                Console.WriteLine("Enter item name to Delete");
+                string del = Console.ReadLine();
+                dc.Delete(del);
+
+                Console.WriteLine("Enter item name to search");
+                string search = Console.ReadLine();
+                dc.getItem(search);
+
+                
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
